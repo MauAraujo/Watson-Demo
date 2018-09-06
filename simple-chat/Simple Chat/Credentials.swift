ba0662f73a985c395b98d603ca0198f516dee7fa
MauAraujo <impendingfern@gmail.com>
2018-09-05 19:46:21 -0700

    Agregar Credentials

---
+++ simple-chat/Simple Chat/Credentials.swift
@@ -0,0 +1,25 @@
+/**
+ * Copyright IBM Corporation 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+ 
+public struct Credentials {
+    public static let AssistantUsername = "11c79bfa-6a04-4877-9d57-6805b86b9841"
+    public static let AssistantPassword = "8JqIbYKaY3Fs"
+    public static let AssistantWorkspace = "b67416dd-ec00-4710-96c5-b7ef618e5e0d"
+    public static let SpeechToTextUsername = "18604605-0baa-4d1e-a868-d81cdf7fb858"
+    public static let SpeechToTextPassword = "SaPThosL23Gn"
+    public static let TextToSpeechUsername = "84f9d76b-6bb1-4f6c-8f45-762bf9a03992"
+    public static let TextToSpeechPassword = "t3Bx068T4OlZ"
+}
